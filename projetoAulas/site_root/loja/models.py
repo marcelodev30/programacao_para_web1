0b3ccf92e567d68f00cfa99361832ce12aa2973d
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-03-15 15:03:12 -0300

    dia 15

---
+++ projetoAulas/site_root/loja/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.nome
